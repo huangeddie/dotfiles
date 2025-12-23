c6e021066a8c403aa091a333b10205b6fc128792
Eddie Huang <aigagror@gmail.com>
2025-12-22 17:27:37 -0800

    initial commit

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cp", function()
+  local path = "@" .. vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, { desc = "Copy relative path" })
+
+vim.keymap.set("n", "<leader>cP", function()
+  local paths = {}
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+      local name = vim.api.nvim_buf_get_name(buf)
+      if name ~= "" then
+        local rel = vim.fn.fnamemodify(name, ":.")
+        table.insert(paths, "@" .. rel)
+      end
+    end
+  end
+  local result = table.concat(paths, " ")
+  vim.fn.setreg("+", result)
+  vim.notify("Copied: " .. result)
+end, { desc = "Copy all buffer paths" })
