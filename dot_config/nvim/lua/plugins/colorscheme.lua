d0929c0b58d707cce34425012fecc1779923032f
Eddie Huang <aigagror@gmail.com>
2026-03-06 18:44:45 -0800

    Add .config/nvim/lua/plugins/blink-cmp.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/example.lua

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "kepano/flexoki-neovim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki-dark",
+    },
+  },
+}
