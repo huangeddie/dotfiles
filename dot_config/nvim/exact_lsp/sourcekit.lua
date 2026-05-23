9dfb3aa599ccbb0d8c781c5f27bcc1c715361654
Eddie Huang <aigagror@gmail.com>
2026-05-23 11:45:34 -0700

    add swift LSP

---
+++ dot_config/nvim/exact_lsp/sourcekit.lua
@@ -0,0 +1,31 @@
+---@brief
+---
+--- https://github.com/swiftlang/sourcekit-lsp
+---
+--- Language server for Swift.
+return {
+	cmd = { "sourcekit-lsp" },
+	filetypes = { "swift" },
+	root_markers = {
+		".git",
+		"compile_commands.json",
+		".sourcekit-lsp",
+		"Package.swift",
+	},
+	get_language_id = function(_, ftype)
+		return ftype
+	end,
+	capabilities = {
+		workspace = {
+			didChangeWatchedFiles = {
+				dynamicRegistration = true,
+			},
+		},
+		textDocument = {
+			diagnostic = {
+				dynamicRegistration = true,
+				relatedDocumentSupport = true,
+			},
+		},
+	},
+}
