32a8fcc50ecef60e2caaeedc79e8ba84e2173129
Eddie Huang <aigagror@gmail.com>
2026-06-14 20:52:05 -0700

    change to gitub dark default

--- dot_config/nvim/lua/exact_plugins/colorscheme.lua
+++ dot_config/nvim/lua/exact_plugins/colorscheme.lua
@@ -9,7 +9,7 @@ return {
 		opts = {
 			set_dark_mode = function()
 				vim.o.background = "dark"
-				vim.cmd.colorscheme("github_dark")
+				vim.cmd.colorscheme("github_dark_default")
 			end,
 			set_light_mode = function()
 				vim.o.background = "light"
