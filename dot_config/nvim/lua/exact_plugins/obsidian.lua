efbc496020598a4e7eaedbc916343f7a16f11ad3
Eddie Huang <aigagror@gmail.com>
2026-07-30 22:05:17 -0700

    update obsidian workspaces

--- dot_config/nvim/lua/exact_plugins/obsidian.lua
+++ dot_config/nvim/lua/exact_plugins/obsidian.lua
@@ -7,11 +7,7 @@ return {
 		legacy_commands = false, -- this will be removed in 4.0.0
 		workspaces = {
 			{
-				name = "personal",
-				path = "~/Documents/Obsidian/main/",
-			},
-			{
-				name = "personal",
+				name = "main",
 				path = "~/Documents/Obsidian/",
 			},
 		},
