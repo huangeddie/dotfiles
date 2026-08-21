b21e1ad7f97bdb171860d1be6383070a3064f6cf
Eddie Huang <aigagror@gmail.com>
2026-08-20 20:48:00 -0700

    chore: update dot_config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.zig" },
+		{ import = "lazyvim.plugins.extras.lang.ruby" },
 		{ import = "plugins" },
 	},
 	defaults = {
