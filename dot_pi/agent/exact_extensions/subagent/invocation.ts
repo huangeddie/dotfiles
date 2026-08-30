87a746a7ab8653d3b18806487fd42db4c5ce519e
Eddie Huang <aigagror@gmail.com>
2026-08-30 16:02:32 -0700

    chore: update dot_pi/agent/exact_extensions/comment.ts

--- dot_pi/agent/exact_extensions/subagent/invocation.ts
+++
@@ -1,6 +0,0 @@
-const SUBAGENT_TOOL_NAME = "subagent";
-
-export function buildChildToolArgs(tools?: readonly string[]): string[] {
-	const args = tools && tools.length > 0 ? ["--tools", tools.join(",")] : [];
-	return [...args, "--exclude-tools", SUBAGENT_TOOL_NAME];
-}
